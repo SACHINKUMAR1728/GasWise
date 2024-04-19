57eb4ff0e783f295286918f3fdd74289fd769165
Aditya bandaru <adityabandaruuu18@gmail.com>
2024-04-19 23:59:53 +0530

    Button  added

--- Frontend/src/Body.jsx
+++ Frontend/src/Body.jsx
@@ -16,6 +16,7 @@ const Body = () => {
         <div className='w-1/2 flex flex-col'>
           <div className="bg-gray-200 text-black px-4 py-2 border-b border-gray-800">
             Enter your Contract here:
+            <button className='p-2 rounded-[10px] bg-blue-500 relative left-[500px] px-4'>Run</button>
           </div>
           <textarea
             className="flex-grow px-4 py-2 text-sm text-white rounded-none border border-gray-800 focus:outline-none bg-gray-800"
