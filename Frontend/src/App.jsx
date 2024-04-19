d382d3d0247c9dc17a8209d8911352b405323abb
Aditya bandaru <adityabandaruuu18@gmail.com>
2024-04-20 04:01:38 +0530

    UI Added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,20 @@
 import "./Body.jsx";
+import Home from "./Home.jsx";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Body from "./Body.jsx";
-
 
 function App() {
 
   return (
     <div className='bg-black'>
-  <Body />
+      <Router>
+        <Routes>
+
+          <Route path="/" element={<Home />} />
+          <Route path="/body" element={<Body />} />
+          
+        </Routes>
+      </Router>
     </div>
   )
 }
